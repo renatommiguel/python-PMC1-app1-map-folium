7f3ec9b6ccefb03c4235f7ed8e4c3774c2ec7138
G14-ROG-LAPTOP\renat <renatommiguel@gmail.com>
2022-06-28 08:00:06 +0200

    commit

---
+++ src/map.py
@@ -0,0 +1,16 @@
+# try out
+import folium
+import time
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def func_test():
+    map = folium.Map(location=[-21.80158482316849, -54.54833021801829], )  # zoom_start=6)
+    #  latitude [-90,90] and longitude [-180,180]
+    map.save(f"{dir_path}/map{int(time.time())}.html")
+
+
+if __name__ == "__main__":
+    func_test()
