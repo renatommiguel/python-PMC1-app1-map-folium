087c3b8e02e23b1cb56cc03da41992251a2b97e5
G14-ROG-LAPTOP\renat <renatommiguel@gmail.com>
2022-06-30 08:03:10 +0200

    deleted app from study branch and add to app branch
    Signed-off-by: G14-ROG-LAPTOP\renat <renatommiguel@gmail.com>

--- src/map.py
+++ src/map.py
@@ -14,7 +14,8 @@ print(f"dir_path:{dir_path}")
 def func_test():
     map = folium.Map(location=[-21.80158482316849, -54.54833021801829], )  # zoom_start=6)
     #  latitude [-90,90] and longitude [-180,180]
-    map.save(f"{dir_path}/map{int(time.time())}.html")
+    map.save(f"{dir_path}/map.html")
+    # map.save(f"{dir_path}/map{int(time.time())}.html")
 
 
 if __name__ == "__main__":
